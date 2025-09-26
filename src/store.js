1899ce4210da89d10458240ede56c95aa9deb19e
madss-fi <madiiculous@gmail.com>
2025-09-26 06:51:05 -0400

    store updted

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
+// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using Redux Toolkit's configureStore
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // 'cart' slice is managed by cartReducer
+  },
 });
-export default store
+
+export default store; // Export the store to use in your App
